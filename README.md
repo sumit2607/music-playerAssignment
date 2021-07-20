5de618090431d19646361dfabb5cc5b4014232f1
Sumit Rai <40994984+sumit2607@users.noreply.github.com>
2021-07-20 09:49:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Music Player
+what I did in making this application is
+* insert supporting images on drawable
+* create raw folders and add a song to that folder
+* make a layout ( play, stop, back, dan forward, Imageview, Seekbar )
+* MediaPlayer
---
+++ app/src/main/java/com/atsdev/musicplayer/MainActivity.java
@@ -0,0 +1,80 @@
+package com.atsdev.musicplayer;
+
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+    private TextView songName, songDuration;
+    private SeekBar seekBar;
+    private double timeStart = 0, finalTime = 0;
+    private int forwardTime = 2000, backwardTime = 2000;
+    private Handler durationHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        songName = (TextView) findViewById(R.id.songName);
+        songDuration = (TextView) findViewById(R.id.songDuration);
+        mediaPlayer = MediaPlayer.create(this, R.raw.lagu);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        songName.setText("Lagu.mp3");
+        seekBar.setMax((int) finalTime);
+        seekBar.setClickable(false);
+    }
+
+    private Runnable updateSeekBarTime = new Runnable() {
+        @Override
+        public void run() {
+            timeStart = mediaPlayer.getCurrentPosition();
+            seekBar.setProgress((int) timeStart);
+            double timeRemaining = finalTime - timeStart;
+            songDuration.setText(String.format("%d min, %d sec",
+                    TimeUnit.MILLISECONDS.toMinutes((long) timeRemaining),
+                    TimeUnit.MILLISECONDS.toSeconds((long) timeRemaining) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long)timeRemaining))));
+            durationHandler.postDelayed(this, 100);
+        }
+    };
+
+    public void play(View view) {
+        mediaPlayer.start();
+        timeStart = mediaPlayer.getCurrentPosition();
+        seekBar.setProgress((int) timeStart);
+        durationHandler.postDelayed(updateSeekBarTime, 100);
+    }
+
+    public void pause(View view) {
+        mediaPlayer.pause();
+    }
+
+    public void forward(View view) {
+        if ((timeStart + forwardTime) <= finalTime) {
+            timeStart = timeStart - backwardTime;
+            mediaPlayer.seekTo((int) timeStart);
+        }
+    }
+
+    public void backforward(View view) {
+        //check if we can go back at backwardTime seconds after song starts
+        if ((timeStart - backwardTime) > 0) {
+            timeStart = timeStart - backwardTime;
+            mediaPlayer.seekTo((int) timeStart);
+        }
+    }
+
+
+
+
+    }
+
+
+
